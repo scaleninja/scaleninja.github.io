708bff69badd499d7d2a0f71b7edbb2d8d981e8b
Rohit Yadav <rohit@yadav.cloud>
2025-12-26 17:10:08 +0530

    initial website
    Signed-off-by: Rohit Yadav <rohit@yadav.cloud>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,43 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeNova from 'starlight-theme-nova';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    integrations: [
+        starlight({
+            title: 'ScaleNinja',
+            plugins: [
+                starlightThemeNova({
+                    nav: [
+                        { label: 'Guides', href: '/guides/cloudstack' },
+                        { label: 'External link', href: 'https://example.com' },
+                    ],
+                }),
+            ],
+            logo: {
+              light: './public/logo.svg',
+              dark: './public/logo-white.svg',
+              replacesTitle: true,
+            },
+            social: [
+                { icon: 'github', label: 'GitHub', href: 'https://github.com/scaleninja' },
+            ],
+            sidebar: [
+                {
+                    label: 'Guides',
+                    autogenerate: { directory: 'guides' },
+                },
+                {
+                    label: 'Reference',
+                    autogenerate: { directory: 'reference' },
+                },
+            ],
+            customCss: ['./src/styles/custom.css'],
+            components: {
+                Footer: './src/components/starlight/Footer.astro',
+            },
+        }),
+    ],
 });
